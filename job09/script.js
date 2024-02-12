cb671ab1df9e17c36080634f1c1a600ea979d438
Jessy Charlet <Jessycharlet@gmail.com>
2024-02-12 16:56:46 +0100

    Job09 2.0
    Ajout de la fonction tri par ordre croissant ou décroissant

--- job09/script.js
+++ job09/script.js
@@ -1,20 +1,31 @@
 
-function tri(nbr){
-    for(var a = 1 ; a <= max ; a++)
-    {
-        for(var b = 0; b <= nbr.length ; b++)
-        {
-        if(nbr[b] == a )
-        {
-            document.write(nbr[b] + "<br/>");
+function tri(nbr) {
+    if (order == "asc") {
+        for (var a = 1; a <= max; a++) {
+            for (var b = 0; b <= nbr.length; b++) {
+                if (nbr[b] == a) {
+                    document.write(nbr[b] + "<br/>");
+                }
+            }
         }
     }
+    else if (order == "desc") {
+        for (var a = max; a >= 1; a--) {
+            for (var b = 0; b <= nbr.length; b++) {
+                if (nbr[b] == a) {
+                    document.write(nbr[b] + "<br/>");
+                }
+            }
+        }
+    }
+    else {
+        document.write("erreur : aucun order selectionné")
     }
 }
 
+//Séléctionnez dans order "asc" pour trier par ordre croissant et "desc" pour trier par ordre décroissant.
 var order = "asc";
-var order = "desc"; 
-var numbers = [42,587,54226,2321,1258,4236,9985,54458,95,1,22,301,542]
+var numbers = [42, 587, 54226, 2321, 1258, 4236, 9985, 54458, 95, 1, 22, 301, 542]
 const max = Math.max(...numbers);
 
 tri(numbers);
