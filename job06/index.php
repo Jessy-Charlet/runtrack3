8bdb9faf955a46953db5b86e85018f79973697de
Jessy Charlet <Jessycharlet@gmail.com>
2024-02-12 13:43:09 +0100

    Job06
    Job 06 terminé

---
+++ job06/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="UTF-8" />
+        <title>Runetrack3</title>
+        <meta name="author" content="Jessy Charlet" />
+        <meta name="description" content="Runetrack3" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
+    <body>
+    <script src="./script.js"></script>
+    </body>
+    </html>
