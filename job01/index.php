0051e9b004098c7a12105ad9485d457d43ea9c2e
Jessy Charlet <Jessycharlet@gmail.com>
2024-02-12 11:14:52 +0100

    Job01
    Job 01 terminé

---
+++ job01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="UTF-8" />
+        <title>Runetrack3</title>
+        <meta name="author" content="Jessy Charlet" />
+        <meta name="description" content="Runetrack3" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
+    <body>
+    <script>
+alert("Hello Javascript !");
+</script>
+    </body>
+    </html>
