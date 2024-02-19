5d7fbaa91d66bc143634ffb3591f0d4f30dd17d2
Jessy Charlet <Jessycharlet@gmail.com>
2024-02-19 17:00:10 +0100

    Jour 06 Job 02
    Job 02 terminé

---
+++ Jour 06/job02/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+const screenS = window.matchMedia("(max-width: 1280px)");
+const screenM = window.matchMedia("(min-width: 1280px) and (max-width: 1680px)");
+const screenL = window.matchMedia("(min-width: 1680px) and (max-width: 1920px)");
+window.addEventListener("resize", function () {
+    if (screenS.matches) {
+        document.body.style.backgroundColor = "#ffffff";
+        }
+    else if(screenM.matches){
+        document.body.style.backgroundColor = "#275fa0";
+    }
+    else if(screenL.matches){
+        document.body.style.backgroundColor = "#ffca4b";
+    }
+});
+});
